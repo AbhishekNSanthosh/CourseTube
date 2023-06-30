b506f9b586adf2ee3ab2fa225c820b4c6f674548
Abhishek <you@example.com>
2023-06-30 21:45:33 +0530

    30-06 update2

--- frontend/src/Components/SigninModal/Signin.jsx
+++ frontend/src/Components/SigninModal/Signin.jsx
@@ -11,7 +11,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '50%',
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -19,8 +19,6 @@ const style = {
 };
 
 export default function TransitionsModal({modalOpen,handleClose}) {
-  const [open, setOpen] = React.useState(false);
-//   const handleOpen = () => setOpen(true);
 
   return (
     <div>
