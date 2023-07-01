bba73013477b5757e3f5041b41d5a0aa48754d38
Abhishek <you@example.com>
2023-07-01 08:09:56 +0530

    1-07 update2

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -8,61 +8,68 @@ import ExploreDrop from '../ExploreDrop';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import TransitionsModal from "../SigninModal/Signin";
+import CreateAccount from "../CreateAccountModal/CreateAccount";
 
 const Navbar = () => {
-  const [modalOpen,setModalOpen] = useState(true)
+  const [modalSignInOpen, setModalSignInOpen] = useState(false);
+  const [modalSignUpOpen, setModalSignUpOpen] = useState(false);
 
 
-const handleClose = () => {
-  setModalOpen(false)
-}
+  const handleClose = () => {
+    setModalSignInOpen(false);
+    setModalSignUpOpen(false)
+  }
+
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
-        <Link to='/' className="link">
-          <img className="logoImg" src={logo} alt="logo"/>
+          <Link to='/' className="link">
+            <img className="logoImg" src={logo} alt="logo" />
           </Link>
         </div>
         <div className="searchContainer">
           <div className="searchBox">
             <div className="search">
-            <TextField
-            placeholder='Want to learn ?'
-              height='20px'
-              sx={{
-                "& fieldset": { border: "none" },
-              }}
-              InputProps={{
-                style: {
-                  height: "40px",
-                  width:'450px'
-                },
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-              </div>
-          <div className="explore">
-            <ExploreDrop/>
-          </div>
+              <TextField
+                placeholder='Want to learn ?'
+                height='20px'
+                sx={{
+                  "& fieldset": { border: "none" },
+                }}
+                InputProps={{
+                  style: {
+                    height: "40px",
+                    width: '450px'
+                  },
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </div>
+            <div className="explore">
+              <ExploreDrop />
+            </div>
           </div>
         </div>
         <div className="signinContainer">
           <div className="sigin">
-            <span className="siginSpan">Signin</span>
+            <span className="siginSpan" onClick={() => {
+              setModalSignInOpen(true);
+            }}>Sign In</span>
           </div>
           <div className="createAccount">
-          <Button variant="contained" onClick={()=>{
-            setModalOpen(true)
-          }}>Creare free Account</Button>
+            <Button variant="contained" onClick={() => {
+              setModalSignUpOpen(true)
+            }}>Create Free Account</Button>
           </div>
         </div>
       </div>
-      <TransitionsModal modalOpen={modalOpen} handleClose={handleClose}/>
+      <TransitionsModal modalSignInOpen={modalSignInOpen} handleClose={handleClose} />
+      <CreateAccount modalSignUpOpen={modalSignUpOpen}/>
     </div>
   );
 };
